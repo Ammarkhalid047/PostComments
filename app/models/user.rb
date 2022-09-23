fb4672d3b7aa1f11502e934e28f9ce3ce578e701
Ammarkhalid047 <ammar.khalid047@gmail.com>
2022-09-23 15:52:45 +0500

    final

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   # has_one_attached :image
   
   has_many :post,  dependent: :destroy
