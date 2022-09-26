314d114d0b1ed6ad8afdac2fbd2979407665aa68
Ammarkhalid047 <ammar.khalid047@gmail.com>
2022-09-26 11:28:07 +0500

    s

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,17 +18,7 @@ Rails.application.configure do
   
   # config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :smtp
-      config.action_mailer.smtp_settings = {
-        address:              'smtp.gmail.com',
-        port:                 587,
-        domain:               'postcomments-ammar.herokuapp.com',
-        user_name:            'ammar.khalid047@gmail.com',
-        password:             '',
-        authentication:       'plain',
-        enable_starttls_auto: true,
-        open_timeout:         5,
-        read_timeout:         5 }
+
 
 
   # Enable/disable caching. By default caching is disabled.
@@ -51,7 +41,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
