44d1ddc8c0c47574545128cc982544ce674099da
Ammarkhalid047 <ammar.khalid047@gmail.com>
2022-09-26 11:52:04 +0500

    resolved mailer issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  host = https://postcomments-ammar.herokuapp.com
+  host = 'https://postcomments-ammar.herokuapp.com'
   config.action_mailer.default_url_options = { host: host}
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
